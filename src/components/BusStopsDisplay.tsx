b0362f0d381260a4c582282ad4802da544359c00
Dan Bonneville <danmbonneville@gmail.com>
2024-12-21 17:36:42 -0600

    fix favicon, add more styling, add a label

--- src/components/BusStopsDisplay.tsx
+++ src/components/BusStopsDisplay.tsx
@@ -9,7 +9,7 @@ const BusStopsDisplay: React.FC<BusStopsDisplayProps> = ({
 
   return (
     <div data-testid={'bus-stops-wrapper'} className="bus-stops-wrapper">
-      <h2>{busStopDisplayLabel}</h2>
+      {busStops.length > 0 && <h2>{busStopDisplayLabel}</h2>}
       {busStops.map((busStop) => (
         <div className="bus-stop-list-item" key={busStop.place_code}>
           <p>{busStop.description}</p>
