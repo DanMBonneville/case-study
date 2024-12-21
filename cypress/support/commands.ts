f695cdad8b0787b5465b7757074c0a774f15b912
Dan Bonneville <danmbonneville@gmail.com>
2024-12-21 14:44:28 -0600

    add remaining e2es, fix bug in the process

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,28 +1,78 @@
 /// <reference types="cypress" />
+import '@testing-library/cypress/add-commands';
 
-import "@testing-library/cypress/add-commands";
+Cypress.Commands.add('selectBusRoute', (route: string) => {
+  cy.findByTestId('route-select').click();
+  cy.findByText(route).click();
+});
 
-// Cypress.Commands.add("selectRouteAndDirection", (params: any) => {
-// 	cy.findByTestId("route-select").click();
-// 	cy.findByText(params.route).click();
-// 	cy.findByTestId("direction-select").click();
-// 	cy.findByText(params.direction).click();
-// });
+Cypress.Commands.add('selectRouteDirection', (direction: string) => {
+  cy.findByTestId('direction-select').click();
+  cy.findByText(direction).click();
+});
 
-// Cypress.Commands.add("customCommand", (param: string) => {
-// 	cy.log(`Custom command with param: ${param}`);
-// });
+Cypress.Commands.add(
+  'selectRouteAndDirection',
+  (route: string, direction: string) => {
+    cy.selectBusRoute(route);
+    cy.selectRouteDirection(direction);
+  }
+);
 
-// declare global {
-// 	namespace Cypress {
-// 		interface Chainable {
-// 			selectRouteAndDirection(
-// 				route: string,
-// 				direction: string
-// 			): Chainable<void>;
-// 			// drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-// 			// dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-// 			// visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-// 		}
-// 	}
-// }
+Cypress.Commands.add('clearRouteSelection', () => {
+  cy.findByTestId('route-select').find('svg').first().click({ force: true });
+});
+
+Cypress.Commands.add('clearDirectionSelection', () => {
+  cy.findByTestId('direction-select')
+    .find('svg')
+    .first()
+    .click({ force: true });
+});
+
+Cypress.Commands.add(
+  'getBusStopsForRouteAndDirection',
+  (route: string, direction: string) => {
+    cy.selectBusRoute(route);
+    cy.selectRouteDirection(direction);
+    cy.findByTestId('bus-stops-wrapper')
+      .find('.bus-stop-list-item p')
+      .then(($pElements) => {
+        return Cypress.$.map($pElements, (el) => (el as HTMLElement).innerText);
+      });
+  }
+);
+
+Cypress.Commands.add(
+  'verifyStringArraysAreDisjoint',
+  (arr1: string[], arr2: string[]) => {
+    arr1.forEach((arr1Element: string) => {
+      arr2.forEach((arr2Element: string) => {
+        cy.wrap(arr1Element).should('not.equal', arr2Element);
+      });
+    });
+  }
+);
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      selectBusRoute(route: string): Chainable<void>;
+      selectRouteDirection(direction: string): Chainable<void>;
+      selectRouteAndDirection(
+        route: string,
+        direction: string
+      ): Chainable<void>;
+      clearRouteSelection(): Chainable<void>;
+      clearDirectionSelection(): Chainable<void>;
+      getBusStopsForRouteAndDirection(
+        route: string,
+        direction: string
+      ): Chainable<string[]>;
+      verifyStringArraysAreDisjoint(
+        arr1: string[],
+        arr2: string[]
+      ): Chainable<void>;
+    }
+  }
+}
--- src/containers/HomePage.tsx
+++ src/containers/HomePage.tsx
@@ -1,103 +1,105 @@
-import React, { useEffect, useMemo } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { SingleValue } from "react-select";
-import BusStopsDisplay from "../components/BusStopsDisplay";
-import CustomSelect from "../components/CustomSelect";
+import React, { useEffect, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { SingleValue } from 'react-select';
+import BusStopsDisplay from '../components/BusStopsDisplay';
+import CustomSelect from '../components/CustomSelect';
 import {
-	getAllRoutes,
-	getBusStops,
-	getPossibleDirections,
-} from "../store/actions/generalActions";
+  getAllRoutes,
+  getBusStops,
+  getPossibleDirections
+} from '../store/actions/generalActions';
 import {
-	resetReduxState,
-	setBusStops,
-	setSelectedDirection,
-	setSelectedRoute,
-} from "../store/reducers/reducer";
-import { store } from "../store/store";
+  resetReduxState,
+  setBusStops,
+  setSelectedDirection,
+  setSelectedRoute
+} from '../store/reducers/reducer';
+import { store } from '../store/store';
 
 const HomePage: React.FC = () => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const {
-		routes,
-		possibleDirections,
-		busStops,
-		selectedRoute,
-		selectedDirection,
-	} = useSelector((state: any) => state.reducer);
+  const {
+    routes,
+    possibleDirections,
+    busStops,
+    selectedRoute,
+    selectedDirection
+  } = useSelector((state: any) => state.reducer);
 
-	useEffect(() => {
-		if (routes.length === 0) {
-			store.dispatch(getAllRoutes());
-		}
-	}, [routes.length]);
+  useEffect(() => {
+    if (routes.length === 0) {
+      store.dispatch(getAllRoutes());
+    }
+  }, [routes.length]);
 
-	const routeOptions = useMemo(
-		() =>
-			routes?.map((route: any) => ({
-				value: route.route_id,
-				label: route.route_label,
-			})),
-		[routes]
-	);
+  const routeOptions = useMemo(
+    () =>
+      routes?.map((route: any) => ({
+        value: route.route_id,
+        label: route.route_label
+      })),
+    [routes]
+  );
 
-	const directionOptions = useMemo(
-		() =>
-			possibleDirections?.map((direction: any) => ({
-				value: direction.direction_id,
-				label: direction.direction_name,
-			})),
-		[possibleDirections]
-	);
+  const directionOptions = useMemo(
+    () =>
+      possibleDirections?.map((direction: any) => ({
+        value: direction.direction_id,
+        label: direction.direction_name
+      })),
+    [possibleDirections]
+  );
 
-	const handleRouteSelectionChange = (
-		selectedOption: SingleValue<SelectOption>
-	) => {
-		if (selectedOption !== null) {
-			dispatch(setSelectedRoute(selectedOption));
-			store.dispatch(getPossibleDirections(selectedOption.value));
-		} else {
-			dispatch(resetReduxState());
-		}
-	};
+  const handleRouteSelectionChange = (
+    selectedOption: SingleValue<SelectOption>
+  ) => {
+    if (selectedOption !== null) {
+      dispatch(setSelectedRoute(selectedOption));
+      dispatch(setSelectedDirection(null));
+      dispatch(setBusStops([]));
+      store.dispatch(getPossibleDirections(selectedOption.value));
+    } else {
+      dispatch(resetReduxState());
+    }
+  };
 
-	const handleDirectionSelectionChange = (
-		selectedOption: SingleValue<SelectOption>
-	) => {
-		dispatch(setSelectedDirection(selectedOption));
-		if (selectedOption !== null) {
-			const queryParams = {
-				routeId: selectedRoute.value,
-				directionId: selectedOption.value,
-			};
-			store.dispatch(getBusStops(queryParams));
-		} else {
-			dispatch(setBusStops([]));
-		}
-	};
+  const handleDirectionSelectionChange = (
+    selectedOption: SingleValue<SelectOption>
+  ) => {
+    dispatch(setSelectedDirection(selectedOption));
+    if (selectedOption !== null) {
+      const queryParams = {
+        routeId: selectedRoute.value,
+        directionId: selectedOption.value
+      };
+      store.dispatch(getBusStops(queryParams));
+    } else {
+      dispatch(setBusStops([]));
+    }
+  };
 
-	return (
-		<div className="home-page-container">
-			<CustomSelect
-				data-testid="route-select"
-				placeholder="Select route"
-				options={routeOptions}
-				selectedValue={selectedRoute}
-				isDisabled={false}
-				handleChange={handleRouteSelectionChange}
-			/>
-			<CustomSelect
-				data-testid="direction-select"
-				placeholder="Select direction"
-				options={directionOptions}
-				selectedValue={selectedDirection}
-				isDisabled={!selectedRoute}
-				handleChange={handleDirectionSelectionChange}
-			/>
-			<BusStopsDisplay busStops={busStops} />
-		</div>
-	);
+  return (
+    <div className="home-page-container">
+      <CustomSelect
+        data-testid="route-select"
+        placeholder="Select route"
+        options={routeOptions}
+        selectedValue={selectedRoute}
+        isDisabled={false}
+        handleChange={handleRouteSelectionChange}
+      />
+      <CustomSelect
+        data-testid="direction-select"
+        placeholder="Select direction"
+        options={directionOptions}
+        selectedValue={selectedDirection}
+        isDisabled={!selectedRoute}
+        handleChange={handleDirectionSelectionChange}
+      />
+      <BusStopsDisplay busStops={busStops} />
+    </div>
+  );
 };
 
 export default HomePage;
